ac63d11156df9ed11509d69237ab983d0990d0fa
alaasahraoui <tipaza.alaa@hotmail.com>
2020-04-16 17:20:41 +0100

    python scripte has been modified and its working +ens table

--- ga_script.py
+++ ga_script.py
@@ -4,27 +4,15 @@ mydb = mysql.connector.connect(
   passwd="",
   database="test"
 )
-
 my_rooms_cursor = mydb.cursor()
- 
-    
-
-
-
-
 my_rooms_cursor.execute("SELECT room_code,room_max FROM rooms")
-
-
-
-
-
-
-
-
-
-
-
-
 my_rooms_list= my_rooms_cursor.fetchall()
 
- 
+
+
+my_enseignants_cursor=mydb.cursor()
+my_enseignants_cursor.execute("SELECT code_ens,nom_ens FROM enseignants")
+my_enseignants_list= my_rooms_cursor.fetchall()
+
+
+
